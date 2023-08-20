02ffc1f72b3cb622587d54abdc59a41af0334a71
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-08-20 14:55:17 -0500

    Complete lab

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// Helper function to retrieve
+function toTitleCase(string) {
+  return string.replace(string[0], string[0].toUpperCase());
 }
+
+// Helper function to retrieve first word in the string.
+function getFirstWord(string) {
+  let spaceIndex = string.indexOf(" ");
+  return (spaceIndex >= 0) ? string.substring(0, spaceIndex + 1) : string;
+}
+
+// Helper function to capitalize the first letter of each word in the string - most time complexity efficient solution.
+function titleCaseString(string) {
+  let str = string;
+  let title = "";
+  while (str.length > 0) {
+    let word = toTitleCase(getFirstWord(str));
+    title += word;
+    str = str.substring(word.length);
+  }
+  return title;
+}
+
+// Test 1 - Solution 1
+const titleCasedX = () => tutorials.map(function (tutorial) {
+  let str = tutorial;
+  let title = "";
+  while (str.length > 0) {
+    let word = toTitleCase(getFirstWord(str));
+    title += word;
+    str = str.substring(word.length);
+  }
+  return title;
+});
+
+// Solution 2
+const titleCased = () => tutorials.map(tutorial => titleCaseString(tutorial));
+
